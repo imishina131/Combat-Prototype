ce119b77955e4319fe415c94a658e0d713d1acbd
GreenIsaiah <ikragland@my.waketech.edu>
2026-03-01 18:06:16 -0500

    Added Health Bar and Script
    Added Health Bar and Script

---
+++ Assets/Scripts/HealthBar Script.cs
@@ -0,0 +1,44 @@
+//using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    private float _MaxHP = 100;
+    private float _currentHP;
+    [SerializeField] private Image _healthBarFill;
+    [SerializeField] private TextMeshProUGUI _HpText;
+    //[SerializeField] private float _fillSpeed;
+    [SerializeField] private Gradient _colored;
+    void Start()
+    {
+        _currentHP = _MaxHP;
+        _HpText.text = " Health: " + _currentHP;
+    }
+    private void Update()
+    {
+        if ( _currentHP <= 0)
+        {
+            Debug.Log("You are Dead");
+            //play death amination, reSpwan player or reload scene (maybe Add a fade to black)?
+        }
+    }
+    public void UpdatingHP (float amount)
+    {
+        _currentHP += amount;
+        _currentHP=Mathf.Clamp(_currentHP,0f, _MaxHP);
+        _HpText.text = " Health: " + _currentHP;
+        UpdateHealthBar();
+    }
+    private void UpdateHealthBar()
+    {
+        float targetFillamount = _currentHP / _MaxHP;
+        _healthBarFill.fillAmount = targetFillamount;
+        //animation for healthBar
+        //_healthBarFill.DOFillAmount(targetFillamount, _fillSpeed);
+        //gradient
+        _healthBarFill.color= _colored.Evaluate(targetFillamount);
+    }
+
+}
