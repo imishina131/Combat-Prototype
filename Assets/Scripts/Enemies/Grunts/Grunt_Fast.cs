4fafc38de03b4f91bf549ee58b8915f402b22691
Cameron <clczysz6797@gmail.com>
2026-03-10 19:14:54 -0400

    Boss, Grunt_Tank, and Grunt_Fast
    created enemy prefabs with ai

---
+++ Assets/Scripts/Enemies/Grunts/Grunt_Fast.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Grunt_Fast : EnemyBehavior
+{
+    public int damage = 1;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        agent.speed = 6f;
+        attackRange = 1.2f;
+        attackCooldown = 0.8f;
+    }
+
+    public override void Attack()
+    {
+        Debug.Log("Fast Enemy Hit Player");
+    }
+}
--- Assets/Scripts/WaveManagerScirpts/Enemy.cs
+++ Assets/Scripts/WaveManagerScirpts/Enemy.cs
@@ -6,6 +6,24 @@ public class Enemy : MonoBehaviour
 {
     public static Action<Enemy> OnEnemyKilled;
 
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
     public void Kill()
     {
         OnEnemyKilled?.Invoke(this);
